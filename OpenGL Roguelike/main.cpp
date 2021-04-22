45a2cdf19e3b4d9a234deeec197b74a7d7ddc01c
Ian Laidlaw <ian@kaboodles.com>
2021-04-22 18:05:14 -0400

    First triangle

--- OpenGL Roguelike/main.cpp
+++ OpenGL Roguelike/main.cpp
@@ -10,6 +10,10 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+GLboolean wireframe = GL_FALSE;
+
+
+//hardcoded shaders, will replace with file loading later
 const char* vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
@@ -48,6 +52,93 @@ int main() {
         return -1;
     }
 
+
+    //Vertex Shader compilation
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    //Fragment shader compilation
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    //Shader program: actually passes the shaders to the GPU
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKER_FAILED\n" << infoLog << std::endl;
+    }
+
+    //deletes the shaders as they are no longer needed on the CPU
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    //vertices for a 2D rectangle
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f, //top right
+        0.5,  -0.5f, 0.0f, //bottom right
+       -0.5f, -0.5f, 0.0f, //bottom left
+       -0.5f,  0.5f, 0.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,    //first triangle
+        1, 2, 3     //second triangle
+    };
+
+    //Vertex Buffer Object: holds all vertex data to send to GPU
+    //Vertex Array Object: holds vertex attributes
+    //Element Buffer Object: Stores an indice that tells OpenGL what sets of vertices to draw
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //Static draw is used since we only need to write the vertices to the GPU once and then render them each frame
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //Tells OpenGL how to read the data from the Vertex Buffer Object
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    
+    //Draw in polygon mode
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
     //render looop
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -56,6 +147,11 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //actually uses the shaders when rendering
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
